9daa3307e67b8a60547a088b21f6fbf36b53cdd3
RafalKornel <rafal.kornel123@gmail.com>
2024-03-02 15:53:43 +0100

    feature: add day tracking

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  static LOCAL_STORAGE_KEY = 'current_day_key';
 
+  currentDay: number;
+
+  constructor() {
+    this.currentDay = this.getCurrentDay() || 0;
+  }
+
+  getCurrentDay() {
+    const value = localStorage.getItem(HomeComponent.LOCAL_STORAGE_KEY);
+
+    if (!value) {
+      return 1;
+    }
+
+    try {
+      const intValue = Number.parseInt(value);
+
+      return intValue;
+    } catch (e) {
+      return 1;
+    }
+  }
+
+  saveCurrentDay(day: number | string) {
+    day = typeof day === 'string' ? Number.parseInt(day) : day;
+    this.currentDay = day;
+
+    localStorage.setItem(HomeComponent.LOCAL_STORAGE_KEY, String(day));
+  }
+
+  completeCurrentDay() {
+    let currentDay = this.getCurrentDay() || 0;
+
+    currentDay++;
+
+    this.saveCurrentDay(currentDay);
+  }
 }
